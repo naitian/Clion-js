c964fcd262accc013266cd6fe2d9ae9b0c557077
Naitian Zhou <nzhou2011@gmail.com>
2016-01-22 12:14:32 -0500

    default os temp directories

--- clion.js
+++ clion.js
@@ -2,7 +2,7 @@
 const VERSION = "0.1.0",
 	DESCRIPTION = "A command line interface for TJHSST Ion",
 	WEEKDAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-	AUTHFILE = "/var/tmp/clion-auth";
+	AUTHFILE = require('os').tmpdir() + "/clion-auth";
 
 const fs = require('fs'),
 	program = require('gitlike-cli'),
